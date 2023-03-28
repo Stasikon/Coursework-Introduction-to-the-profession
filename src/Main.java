6851287dd0854c0d0783fbf1bd95be69b3d6eb15
Stasikon <lecsa515@gmail.com>
2023-03-28 15:40:53 +0300

    test commit1

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+        Emploe[] emploe = new Emploe[3];
+
+        emploe[0] = new Emploe("Pol", 2, 20000);
+        emploe[1] = new Emploe("Pol2", 1, 90000);
+        emploe[2] = new Emploe("Pol3", 3, 50000);
+        printAllEmploe(emploe);
+    }
+
+    public static void printAllEmploe(Emploe[]emploes) {
+        for (Emploe emploe : emploes) {
+            System.out.println(emploe);
+        }
+    }
+
+}
